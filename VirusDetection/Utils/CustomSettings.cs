a3e96dc70add33bba2b5c183edd9c3230af4e225
xuanhvh <xuanhvh@gmail.com>
2016-01-08 09:36:04 +0700

    + Update settings

--- VirusDetection/Utils/CustomSettings.cs
+++ VirusDetection/Utils/CustomSettings.cs
@@ -7,12 +7,15 @@ namespace VirusDetection.Utils
 {
     public class CustomSettings
     {
-        public static String VIRUS_FOLDER = @"..\..\TestVirus\Virus";
-        public static String BENIGN_FOLDER = @"..\..\TestVirus\Benign";
-        public static String DETECTOR_FILE  = @"..\..\TestVirus\Detector.txt";
-        public static String MIX_DETECTOR_FILE = @"..\..\TestVirus\MixDetector.txt";
-        public static String CLUSTERING_FILE  = @"..\..\TestVirus\Clustering.txt";
-        public static String FILE_CLASSIFIER_FILE = @"..\..\TestVirus\FileClassifier.txt";
-        public static String TEST_FILE_FOLDER = @"..\..\TestVirus\TestFile";
+        public static String TEST_FOLDER = @"..\..\TestData\";
+
+        public static String VIRUS_FOLDER = TEST_FOLDER+@"\TestVirus\Virus\";
+        public static String BENIGN_FOLDER = TEST_FOLDER + @"\TestVirus\Benign\";
+        public static String TEST_FILE_FOLDER = TEST_FOLDER + @"\TestVirus\TestFile\";
+
+        public static String DETECTOR_FILE = TEST_FOLDER + @"\TestVirus\Detector.txt";
+        public static String MIX_DETECTOR_FILE = TEST_FOLDER + @"\TestVirus\MixDetector.txt";
+        public static String CLUSTERING_FILE = TEST_FOLDER + @"\TestVirus\Clustering.txt";
+        public static String FILE_CLASSIFIER_FILE = TEST_FOLDER + @"\TestVirus\FileClassifier.txt";
     }
 }
